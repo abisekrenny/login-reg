0a41d94f5d40197c183e73ce2292dd54fbcc83fd
J Abisek Renny <jabisekrenny@gmail.com>
2024-05-26 22:43:56 +0530

    Modified login page to look like the wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Login } from './Login'
-import { Register } from './Register'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Login } from "./Login";
+import { Register } from "./Register";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [currentform, setCurrentForm] = useState('login');
+  const [currentform, setCurrentForm] = useState("login");
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
 
   return (
-      <div className='App'>
-        <div className='container'>
-          <div className='col'>
-          {
-          currentform === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
-        }
-          </div>
-          <div className='col'></div>
+    <div className="container-fluid">
+      <div className="row align-items-center justify-content-between">
+        <div className="col">
+          {currentform === "login" ? (
+            <Login onFormSwitch={toggleForm} />
+          ) : (
+            <Register onFormSwitch={toggleForm} />
+          )}
         </div>
-       
+        <div className="col img-bg">
+          <img src="30a38887b0559dfb619dc9eba940a887.jpeg" alt="" />
+        </div>
       </div>
-     
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './Login.css'
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
@@ -10,11 +11,12 @@ export const Login = (props) => {
     }
     return (
         <div className="auth-form-container">
-            <h2>Login</h2>
+            <h2>Welcome Back</h2>
+            <ph>Today is a new day.It's your day.You shape it.<br/>Sign in to managing your projects.</ph>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
+                <label htmlFor="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
-                <label htmlFor="password">password</label>
+                <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="***yourpassword***" id="password" name="password"/>
                 <button type="submit">Login</button>
             </form>
